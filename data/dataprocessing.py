762d62a6bef89a92091b9f54d1de50cf4d45879f
nagopi <nagopi@iu.edu>
2023-12-05 12:24:57 -0500

    updated data processing, need to fix missing data values

--- data/dataprocessing.py
+++ data/dataprocessing.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import glob
 
-xl = pd.ExcelFile('C:/Users/nandh/Desktop/B365 project/nbadraftprediction/collegestats.xlsx')
+#xl = pd.ExcelFile('C:/Users/nandh/Desktop/B365 project/nbadraftprediction/collegestats.xlsx')
+#for sheet_name in xl.sheet_names:
+#    df = xl.parse(sheet_name)
+#    df.to_csv(f'{sheet_name}.csv', index=False)
 
-for sheet_name in xl.sheet_names:
-    df = xl.parse(sheet_name)
-    df.to_csv(f'{sheet_name}.csv', index=False)
+files = glob.glob('C:/Users/nandh/Desktop/B365 project/nbadraftprediction/data/*.csv')
+dfs = []
+
+for file in files:
+    df = pd.read_csv(file)
+    dfs.append(df)
+
+combined_df = pd.concat(dfs, ignore_index=True)
+print(combined_df)
