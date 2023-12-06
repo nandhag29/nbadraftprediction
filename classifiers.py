cf945d12643120909d06786eb2a8662834e54cd8
nagopi <nagopi@iu.edu>
2023-12-05 21:39:29 -0500

    added classifiers, combined with webscraper

--- classifiers.py
+++ classifiers.py
@@ -22,17 +22,21 @@ data = pd.concat(dfs, ignore_index=True)
 datatokeep = ['NAME','POS','GP','MIN','PTS','FGM','FGA','FG%','3PM','3PA','3P%','FTM','FTA','FT%','REB','AST','Drafted?','Draft Pick','Draft Year']
 data = data[datatokeep]
 data['Drafted?'] = data['Drafted?'].fillna('No')
+data['Draft Pick'] = data['Draft Pick'].fillna(0)
+data['Draft Year'] = data['Draft Year'].fillna(0)
 mapping_dict = {'G': 1, 'F': 2, 'C': 3, 'G-F': 4, 'F-C': 5, 'F-G': 6, 'C-F': 7, 'C-G': 8, 'G-C': 9, 'SG': 10, 'SF': 10}
 data['POS'] = data['POS'].map(mapping_dict)
 mapping_dict1 = {'Yes': 1, 'No': 2}
 data['Drafted?'] = data['Drafted?'].map(mapping_dict1)
+
+data = data.dropna()
 print(data)
 
 ##############
 # CLASSIFIERS
 ##############
 
-feature_cols = ['POS','GP','MIN','PTS','FGM','FGA','FG%','3PM','3PA','3P%','FTM','FTA','FT%','REB','AST']
+feature_cols = ['GP','MIN','PTS','FGM','FGA','FG%','3PM','3PA','3P%','FTM','FTA','FT%','REB','AST']
 label_col = 'Drafted?'
 
 X = data[feature_cols]
@@ -46,65 +50,8 @@ gnb = GaussianNB()
 gnb.fit(X_train, y_train)
 
 print("KNN feature importances:")
-print(knn.feature_importances_)
+print(knn)
 print("GNB feature importances:")
-print(gnb.feature_importances_)
+print(gnb)
 
 
-# hardcoded classifiers
-class hardcodedKNN:
-    def __init__(self, k):
-        self.k = k
-
-    def fit(self, X, y):
-        self.X_train = X
-        self.y_train = y
-
-    def predict(self, X):
-        predicted_labels = [self._predict(x) for x in X]
-        return np.array(predicted_labels)
-
-    def _predict(self, x):
-        distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
-        k_indices = np.argsort(distances)[:self.k]
-        k_nearest_labels = [self.y_train[i] for i in k_indices]
-        most_common = Counter(k_nearest_labels).most_common(1)
-        return most_common[0][0]
-    
-
-class hardcodedNaiveBayes:
-    def fit(self, X, y):
-        self.classes = np.unique(y)
-        self.parameters = []
-        for i, c in enumerate(self.classes):
-            X_c = X[np.where(y == c)]
-            self.parameters.append([])
-            for col in X_c.T:
-                parameters = {"mean": col.mean(), "var": col.var()}
-                self.parameters[i].append(parameters)
-
-    def _pdf(self, class_idx, x):
-        output = 1
-        parameters = self.parameters[class_idx]
-        for i in range(len(parameters)):
-            mean = parameters[i]["mean"]
-            var = parameters[i]["var"]
-            numerator = np.exp(-(x[i]-mean)**2 / (2 * var))
-            denominator = np.sqrt(2 * np.pi * var)
-            output *= numerator / denominator
-        return output
-
-    def _predict(self, x):
-        posteriors = []
-
-        for i, c in enumerate(self.classes):
-            prior = np.log(1 / len(self.classes))
-            posterior = np.sum(np.log(self._pdf(i, x)))
-            posterior = prior + posterior
-            posteriors.append(posterior)
-            
-        return self.classes[np.argmax(posteriors)]
-
-    def predict(self, X):
-        y_pred = [self._predict(x) for x in X]
-        return np.array(y_pred)
--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,93 @@
 import pandas as pd
+import glob
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from collections import Counter
+
+##################
+# DATA PROCESSING
+##################
+
+files = glob.glob('C:/Users/nandh/Desktop/B365 project/nbadraftprediction/data/*.csv')
+dfs = []
+
+for file in files:
+    df = pd.read_csv(file)
+    dfs.append(df)
+
+data = pd.concat(dfs, ignore_index=True)
+
+datatokeep = ['NAME','POS','GP','MIN','PTS','FGM','FGA','FG%','3PM','3PA','3P%','FTM','FTA','FT%','REB','AST','Drafted?','Draft Pick','Draft Year']
+data = data[datatokeep]
+data['Drafted?'] = data['Drafted?'].fillna('No')
+data['Draft Pick'] = data['Draft Pick'].fillna(0)
+data['Draft Year'] = data['Draft Year'].fillna(0)
+mapping_dict = {'G': 1, 'F': 2, 'C': 3, 'G-F': 4, 'F-C': 5, 'F-G': 6, 'C-F': 7, 'C-G': 8, 'G-C': 9, 'SG': 10, 'SF': 10}
+data['POS'] = data['POS'].map(mapping_dict)
+mapping_dict1 = {'Yes': 1, 'No': 0}
+data['Drafted?'] = data['Drafted?'].map(mapping_dict1)
+
+data = data.dropna()
+print(data)
+
+################
+# NEW DATAPOINT
+################
 
 def scrape_sports_reference_player_stats(url):
     tables = pd.read_html(url)
-    totals_table = tables[2]
+    totals_table = tables[1]
     return totals_table
 
-url = 'https://www.sports-reference.com/cbb/players/trey-galloway-1.html'
+url = 'https://www.sports-reference.com/cbb/players/brandon-miller-3.html'
 stats = scrape_sports_reference_player_stats(url)
+stats = stats[['G', 'MP', 'PTS', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'TRB', 'AST']]
+stats.columns = ['GP', 'MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'REB', 'AST']
+stats = stats.iloc[-2]
+stats = stats.to_frame().T
+
 print(stats)
+
+
+##############
+# CLASSIFIERS
+##############
+
+feature_cols = ['GP','MIN','PTS','FGM','FGA','FG%','3PM','3PA','3P%','FTM','FTA','FT%','REB','AST']
+label_col = 'Drafted?'
+
+#K-Nearest Neighbors
+knn = KNeighborsClassifier(n_neighbors=14)
+
+X = data[feature_cols]
+y = data[label_col]
+knn.fit(X, y)
+
+new_data = stats[feature_cols]
+
+prediction = knn.predict(new_data)
+
+print(prediction)
+
+#Naive Bayes
+gnb = GaussianNB()
+
+gnb.fit(X, y)
+
+predictions = gnb.predict(new_data)
+
+print(predictions)
+
+#Logistic Regression 
+lr = LogisticRegression()
+
+lr.fit(X, y)
+
+new_data = stats[feature_cols]
+
+predictions1 = lr.predict(new_data)
+
+print(predictions1)
